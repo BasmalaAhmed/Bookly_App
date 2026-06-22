e1c7f85bb39f0f851c61528bca331f6bf038d18f
BasmalaAhmed9 <basmalaahmedfathi@gmail.com>
2026-06-23 00:47:12 +0300

    Chore : Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:bookly_app/features/forgot_password/presentation/views/forgot_pa
 import 'package:bookly_app/features/login/presentation/views/login_view.dart';
 import 'package:bookly_app/features/register/presentation/views/register_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const BooklyApp());
 }
 
